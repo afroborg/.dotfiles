ba4fdb1dbba27fbb0a5da88c23e6c81343bffc75
Axel Froborg <axel.froborg@gmail.com>
2024-05-14 23:30:58 +0200

    remove unused plugin

--- .config/nvim/lua/afroborg/lazy.lua
+++ .config/nvim/lua/afroborg/lazy.lua
@@ -1,42 +1,40 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    }
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { import = 'afroborg.plugins' },
-    { import = 'afroborg.plugins.lsp' },
-    { 'nvim-lua/plenary.nvim' },
-    { 'HiPhish/rainbow-delimiters.nvim' },
-    { 'tpope/vim-fugitive' },
-    { 'mbbill/undotree' },
-    { 'tpope/vim-surround' },
+  { import = 'afroborg.plugins' },
+  { import = 'afroborg.plugins.lsp' },
+  { 'HiPhish/rainbow-delimiters.nvim' },
+  { 'tpope/vim-fugitive' },
+  { 'mbbill/undotree' },
 }
 
 local opts = {
-    checker = {
-        enable = true,
-        notify = false
-    },
-    change_detection = {
-        notify = false,
-    }
+  checker = {
+    enable = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 }
 
 local status, lazy = pcall(require, 'lazy')
 
 if not status then
-    return
+  return
 end
 
 return lazy.setup(plugins, opts)
